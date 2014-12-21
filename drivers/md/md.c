15132da99464f2cfa33468d8f5dd13ce9290dd8f
Heinz Mauelshagen <heinzm@redhat.com>
2014-12-21 11:50:55 +0100

    dm-raid: initial WIP release supporting takeoover/reshaping/resizing

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -4076,12 +4076,15 @@ static int update_size(struct mddev *mddev, sector_t num_sectors);
 
 <<<<<<< master
 <<<<<<< master
+<<<<<<< master
 /* API to expose size_store() to dm-raid target */
 =======
 >>>>>>> HEAD~77
 =======
 /* API to expose size_store() to dm-raid target */
 >>>>>>> HEAD~70
+=======
+>>>>>>> HEAD~61
 int md_resize(struct mddev *mddev, sector_t sectors)
 {
 	int err;
@@ -4114,12 +4117,15 @@ EXPORT_SYMBOL_GPL(md_resize);
 
 <<<<<<< master
 <<<<<<< master
+<<<<<<< master
 /* Compatibility wrapper around md_resize() to keep md internal inbterface */
 =======
 >>>>>>> HEAD~77
 =======
 /* Compatibility wrapper around md_resize() to keep md internal inbterface */
 >>>>>>> HEAD~70
+=======
+>>>>>>> HEAD~61
 static ssize_t
 size_store(struct mddev *mddev, const char *buf, size_t len)
 {
