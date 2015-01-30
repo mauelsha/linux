2d0e45cbacdaeb66bd011399bf544a683ff44e91
Heinz Mauelshagen <heinzm@redhat.com>
2015-01-30 16:54:11 +0100

    is_divisable_by_data_devs() sets false dev_sectors in case of raid5 -> raid0

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -3606,9 +3606,12 @@ level_store(struct mddev *mddev, const char *buf, size_t len)
 	}
 <<<<<<< master
 <<<<<<< master
+<<<<<<< master
 	if (mddev->queue)
 		blk_set_stacking_limits(&mddev->queue->limits);
 =======
+=======
+>>>>>>> HEAD~59
 <<<<<<< HEAD
 	blk_set_stacking_limits(&mddev->queue->limits);
 >>>>>>> HEAD~75
@@ -3623,13 +3626,22 @@ level_store(struct mddev *mddev, const char *buf, size_t len)
 	pers->run(mddev);
 	set_bit(MD_CHANGE_DEVS, &mddev->flags);
 	mddev_resume(mddev);
+=======
+	if (mddev->queue)
+		blk_set_stacking_limits(&mddev->queue->limits);
+	pers->run(mddev);
+	set_bit(MD_CHANGE_DEVS, &mddev->flags);
+>>>>>>> is_divisable_by_data_devs() sets false dev_sectors in case of raid5 -> raid0
 	if (!mddev->thread)
 		md_update_sb(mddev, 1);
 	sysfs_notify(&mddev->kobj, NULL, "level");
 	md_new_event(mddev);
+<<<<<<< HEAD
 	rv = len;
 out_unlock:
 	mddev_unlock(mddev);
+=======
+>>>>>>> is_divisable_by_data_devs() sets false dev_sectors in case of raid5 -> raid0
 	return rv;
 }
 
