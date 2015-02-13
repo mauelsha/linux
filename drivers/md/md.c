c323925972f1b63818c4712d4c31bbafa98c9d4c
Heinz Mauelshagen <heinzm@redhat.com>
2015-02-13 18:28:36 +0100

    dm-raid/md-raid: fixup rebase issues

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -4188,9 +4188,13 @@ size_show(struct mddev *mddev, char *page)
 static int update_size(struct mddev *mddev, sector_t num_sectors);
 
 <<<<<<< master
+<<<<<<< master
 /* API to expose size_store() to dm-raid target */
 =======
 >>>>>>> HEAD~77
+=======
+/* API to expose size_store() to dm-raid target */
+>>>>>>> HEAD~70
 int md_resize(struct mddev *mddev, sector_t sectors)
 {
 	int err;
@@ -4218,9 +4222,13 @@ int md_resize(struct mddev *mddev, sector_t sectors)
 EXPORT_SYMBOL_GPL(md_resize);
 
 <<<<<<< master
+<<<<<<< master
 /* Compatibility wrapper around md_resize() to keep md internal inbterface */
 =======
 >>>>>>> HEAD~77
+=======
+/* Compatibility wrapper around md_resize() to keep md internal inbterface */
+>>>>>>> HEAD~70
 static ssize_t
 size_store(struct mddev *mddev, const char *buf, size_t len)
 {
