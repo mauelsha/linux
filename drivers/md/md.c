7a9409221aeb55fd74113247d610e24055138cc9
Heinz Mauelshagen <heinzm@redhat.com>
2015-02-13 14:53:21 +0100

    fix 3.19 rebase conflicts

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -3496,6 +3496,7 @@ level_store(struct mddev *mddev, const char *buf, size_t len)
 		del_timer_sync(&mddev->safemode_timer);
 	}
 <<<<<<< master
+<<<<<<< master
 	if (mddev->queue)
 		blk_set_stacking_limits(&mddev->queue->limits);
 =======
@@ -3506,21 +3507,20 @@ level_store(struct mddev *mddev, const char *buf, size_t len)
 	set_bit(MD_CHANGE_DEVS, &mddev->flags);
 	mddev_resume(mddev);
 =======
+=======
+>>>>>>> HEAD~71
 	if (mddev->queue)
 		blk_set_stacking_limits(&mddev->queue->limits);
 	pers->run(mddev);
 	set_bit(MD_CHANGE_DEVS, &mddev->flags);
->>>>>>> is_divisable_by_data_devs() sets false dev_sectors in case of raid5 -> raid0
+	mddev_resume(mddev);
 	if (!mddev->thread)
 		md_update_sb(mddev, 1);
 	sysfs_notify(&mddev->kobj, NULL, "level");
 	md_new_event(mddev);
-<<<<<<< HEAD
 	rv = len;
 out_unlock:
 	mddev_unlock(mddev);
-=======
->>>>>>> is_divisable_by_data_devs() sets false dev_sectors in case of raid5 -> raid0
 	return rv;
 }
 
